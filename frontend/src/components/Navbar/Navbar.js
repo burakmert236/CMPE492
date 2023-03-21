c2246a5621a33ab5c179ceada5e1cfa4ebe5016f
Burak Mert <burakmert236@gmail.com>
2023-03-21 23:19:04 +0300

    initial and functionless version of the navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Dropdown, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 import OptimizeDropdown from "../OptimizeDropdown/OptimizeDropdown";
 
@@ -11,17 +13,56 @@ import "./Navbar.scss";
 //  - Optimize dropdown will be right most part of the navbar
 
 const Navbar = () => {
+    const generateExportLines = (type, index) => {
+        return {
+            key: `1-${index}`,
+            label: (
+                <div className="export-type-line">as {type}</div>
+            )
+        }
+    };
+
+    console.log(["jSON", "SVG", "PNG", "STM"].map((abb, index) => generateExportLines(abb, index)))
+
     return(
         <div className="navbar">
-            <div> 
-                Button section
+            <div className="navbar-buttons"> 
+                <div className="navbar-button logo">LOGO</div>
+
+                <div className="navbar-button">
+                    <Dropdown
+                        menu={{
+                            items: [{
+                                key: 1,
+                                label: "Export",
+                                children: ["jSON", "SVG", "PNG", "STM"].map((abb, index) => generateExportLines(abb, index)),
+                            }]
+                        }}
+                    >
+                        <Space>
+                            File
+                            <DownOutlined />
+                        </Space>
+                    </Dropdown>
+                </div>
+
+                <div className="navbar-button">OPTIONS</div>
+
+                <div className="navbar-button">HELP</div>
             </div>
 
-            <div>
-                Redo / Undo
-            </div>
+            <div className="right-navbar">
+                <div className="redo-undo">
+                    <div className="undo">
+                        <div className="triangle right"></div>
+                    </div>
+                    <div className="redo">
+                        <div className="triangle left"></div>
+                    </div>
+                </div>
 
-            <OptimizeDropdown />
+                <OptimizeDropdown />
+            </div>
 
         </div>
     );
