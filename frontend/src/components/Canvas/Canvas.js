286493eb8354c8facb92a580d16c4026f51bb1fe
Hasan Can Erol <hasan.erol@boun.edu.tr>
2023-03-26 17:43:27 +0300

    GoJS initialization

--- frontend/src/components/Canvas/Canvas.js
+++ frontend/src/components/Canvas/Canvas.js
@@ -1,11 +1,13 @@
-import React from "react";
-
+import React, { useRef, useEffect } from "react";
 import "./Canvas.scss";
 
-const Canvas = () => {
+
+const Canvas = ({diagramRef}) => {
+
     return(
         <div className="canvas-container">
             Canvas
+            <div ref={diagramRef} style={{ height: "100%", width: "100%"}}></div>
         </div>
     );
 };
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,12 +1,26 @@
 import React from "react";
-
+import { useRef, useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Toolbar from "../../components/Toolbar/Toolbar";
 import Canvas from "../../components/Canvas/Canvas";
+import * as go from "gojs";
 
 import "./HomePage.scss";
 
 const HomePage = () => {
+    const diagramRef = useRef(null);
+    useEffect(() => {
+        const diagram = go.GraphObject.make(go.Diagram, diagramRef.current, {initialContentAlignment: go.Spot.Center,
+            'undoManager.isEnabled': true,});
+
+        diagram.commandHandler = new go.CommandHandler();
+        
+        diagram.model = new go.GraphLinksModel(
+            [{ key: "Hello" },   // two node data, in an Array
+             { key: "World!" }],
+            [{ from: "Hello", to: "World!"}]  // one link data, in an Array
+          );
+      }, []);
     return (
         <div className="homepage-layout">
             
@@ -16,7 +30,7 @@ const HomePage = () => {
 
                 <Toolbar />
 
-                <Canvas />
+                <Canvas diagramRef={diagramRef}/>
 
             </div>
 
